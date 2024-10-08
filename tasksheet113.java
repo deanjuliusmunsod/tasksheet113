5fb7d4cc83c3df369eb186696f6503c2b0af8e48
deanjuliusmunsod <djdeanjulius@gmail.com>
2024-10-08 08:29:52 +0000

    done tasksheet113

--- tasksheet113.java
+++ tasksheet113.java
@@ -1,1 +1,22 @@
 
+ class PrimitiveConcatenation {
+
+    public static void main(String[] args) {
+        
+        // Declare and initialize variables
+        char letterH = 'H';
+        int numThree = 3;
+        int numOne = 1;
+        int numZero = 0;
+        String space = " ";
+        char letterW = 'w';
+        float numTwoPointZero = 2.0f;
+        boolean isTrue = true;
+
+        // Concatenate variables to create the desired output
+        String output = "" + letterH + numThree + numOne + numOne + numZero + space + letterW + numZero + "r" + numOne + "d " + numTwoPointZero + " " + isTrue;
+
+        // Print the output
+        System.out.println(output);
+    }
+}
